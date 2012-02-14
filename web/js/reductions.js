d4152ebda08ecd7f390b4a675e783db62ae246df
Daniel Thornton <daniel@relud.com>
2012-02-14 09:46:18 -0800

    add reduction skeleton functions

---
+++ web/js/reductions.js
@@ -0,0 +1,30 @@
+function series_test (resistors) {
+	;
+}
+function series_reduce (resistors) {
+	;
+}
+function parallel_test (resistors) {
+	;
+}
+function parallel_reduce (resistors) {
+	;
+}
+function resistor_test (resistors) {
+	;
+}
+function resistor_reduce (resistors) {
+	;
+}
+function delta_wye_test (resistors) {
+	;
+}
+function delta_wye_reduce (resistors) {
+	;
+}
+function wye_delta_test (resistors) {
+	;
+}
+function wye_delta_reduce (resistors) {
+	;
+}
